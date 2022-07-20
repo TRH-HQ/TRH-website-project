de058a40f2bcc07b4a37efe670503284e01b1d8d
Okpainmo <okpainmoandrew@gmail.com>
2022-07-20 16:24:55 +0100

    ci(next.js): next.js installation completed

---
+++ app_client/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+  return <div className="text-blue-600">Hello world</div>;
+}
+
+const x = 100;
+
+console.log(x);
+
+const test = (a, b) => {
+  return a + b;
+};
