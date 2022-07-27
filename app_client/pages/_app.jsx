e3ed245d74a73427f2d453e515ea5ec6a8897778
Okpainmo <okpainmoandrew@gmail.com>
2022-07-27 13:56:50 +0100

    refactor(app.js file): attempts to add some contexts not successful

--- app_client/pages/_app.jsx
+++ app_client/pages/_app.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+// import ThemeContext from '../contexts/ThemeContext';
+// import NavbarTypeContext from '../contexts/NavbarTypeContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="custom-background custom-default-text-color nunito-sans">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
