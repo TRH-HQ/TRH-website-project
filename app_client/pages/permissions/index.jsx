709ece02a8466f96c74da0540e86fd1f1ed1f734
Okpainmo <okpainmoandrew@gmail.com>
2022-07-27 14:14:04 +0100

    feat(permissions-page): permissions page added

---
+++ app_client/pages/permissions/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import PageRepeatStack from '../../components/PageRepeatStack';
+import PermissionsText from './components/PermissionsText';
+
+function PermissionsPage() {
+  const isStickyNav = true;
+
+  return (
+    <main className="permissions-page">
+      <Navbar isStickyNav={isStickyNav} />
+      <div className="page-header text-5xl pt-[100px] montserrat text-center font-bold">
+        Permissions
+      </div>
+      <div className="w-3/5 mx-auto">
+        <PermissionsText />
+      </div>
+      <PageRepeatStack />
+    </main>
+  );
+}
+
+export default PermissionsPage;
