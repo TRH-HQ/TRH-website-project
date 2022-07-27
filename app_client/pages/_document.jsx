ebe0c5e9ad142fbed092ebd25d42804e5a1a3d1b
Okpainmo <okpainmoandrew@gmail.com>
2022-07-27 14:00:45 +0100

    refactor: i updated google fonts links in the custom next.js document file

--- app_client/pages/_document.jsx
+++ app_client/pages/_document.jsx
@@ -14,7 +14,7 @@ function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Nunito+Sans:wght@400;700&family=Poppins:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Montserrat:wght@400;700&family=Nunito+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
